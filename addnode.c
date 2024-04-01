f97be9cf242056fc7bf09c3c51a59fa5e3b32743
Sholawasiu <sholawasiu92@gmail.com>
2024-04-01 09:40:49 -0700

    newnode

---
+++ addnode.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * addnode - add node to the head stack
+ * @head: head of the stack
+ * @n: new_value to be added
+ * Return: no return
+ */
+
+void addnode(stack_t **head, int n)
+{
+	stack_t *newnode, aux;
+
+	aux = *head;
+	newnode = (stack_t*)malloc(sizeof(stack_t));
+	if (newnode == null)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+	if (aux)
+		aux ->prev = newnode;;
+	newnode->n = n;
+	newnode->next = *head;
+	newnode->prev = NULL;
+	*head = newnode;
+}
