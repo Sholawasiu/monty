7b6f230f9cda593c38483ca249ed8cc28f6e8a7f
Sholawasiu <sholawasiu92@gmail.com>
2024-04-01 09:32:52 -0700

    stack

---
+++ stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * f_stack - prints the top of the stack.
+ * @head: stack head
+ * @count: line_number
+ * Return: no return
+ */
+
+void f_stack(stack_t **head, unsigned int count)
+{
+	(void)head;
+	(void)count;
+	bus.lifi = 0;
+}
+
