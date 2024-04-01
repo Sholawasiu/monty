7457434e5cd593c86590844bfebbc2a1216d2686
Sholawasiu <sholawasiu92@gmail.com>
2024-04-01 06:24:25 -0700

    nop

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ *f_nope- nothing
+ *@head: stack head
+ *@counter: line number
+ *Return: no return
+ */
+
+void f_nop(stack_t **head, unsighned int counter)
+{
+	(void) counter;
+	(void) head;
+}
